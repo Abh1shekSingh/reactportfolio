0d2a917c400c07707aeb24b3881185b2f84b8f6a
Abhishek SIngh <71981463+Abh1shekSingh@users.noreply.github.com>
2022-05-30 16:05:41 +0530

    header changed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,8 +8,8 @@ const Hero = () => {
     <>
       <Header setNavbar = {setNavbar} navbar ={navbar} />
       
-        <div className={`container md:mt-16 ${navbar ? 'Hero' : null}`}>
-          <div className='py-10 px-14 md:px-48'>
+        <div className={`container md:pt-40 pt-20 ${navbar ? 'dsl' : null}`}>
+          <div className='py-10 px-10 md:px-48'>
             <h1 className='font-league text-5xl leading-tight font-black text-white md:text-7xl md:leading-tight'>Hi !<br></br> <span className='text-green'>I'm Abhishek</span> <br></br> <span className='text-light'>I Turn Rasmalai Into Code</span></h1>
             <p className='font-inter font-medium leading-8 text-white text-xl my-4 md:text-xl'>I'm <span className='text-green'>Web Developer</span> based in India <span className='md:hidden'>🇮🇳</span></p>
             <button className='transtition duration-300 bg-transparent mt-6 hover:bg-green text-xl text-white font-fira font-medium py-2 px-6 border border-green hover:border-transparent md:text-sm md:mt-6 md:px-11 md:py-3'>Resume</button>
